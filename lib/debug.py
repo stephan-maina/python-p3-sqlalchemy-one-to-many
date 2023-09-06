81e1a87732ae1ba91f53c037d03298850d9542e5
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 11:00:01 +0300

    completed.

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
+from models import Base, engine
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from models import Game, Review
-
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    import ipdb; ipdb.set_trace()
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
